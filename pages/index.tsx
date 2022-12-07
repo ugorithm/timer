8c83d939fb808f14d8f541b6f266428f530a970b
Hirad Fakouri <86254743+HiradFakouri@users.noreply.github.com>
2022-12-07 10:23:22 +0000

    button design

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ const Home: NextPage = () => {
 
   let time = "Focus Time";
 
-
   useEffect(() => {
     let interval = setInterval(() => {
         clearInterval(interval);
@@ -52,6 +51,8 @@ const Home: NextPage = () => {
       <h1>Timer</h1>
       <p>{time}</p>
       <p>{fminutes}:{fSeconds}</p>
+      <button className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">start</button>
+
     </>
   )
 }
