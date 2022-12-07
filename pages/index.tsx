4454383d19913c5438357291b6d38f1ae311dedf
Hirad Fakouri <86254743+HiradFakouri@users.noreply.github.com>
2022-12-07 16:23:10 +0000

    start and stop but it has a bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,56 +2,68 @@ import type { NextPage } from 'next'
 import React, { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-  const mins = 24;
-  const secs = 60;
-  const breakT = false;
+  //all the hooks
+  let [minutes, setMinutes] = useState(0);
+  let [seconds, setSeconds] = useState(0);
+  let [breakTime, setBreak] = useState(false);
+  let [start, setStart] = useState(false)
 
-  let [minutes, setMinutes] = useState(mins);
-  let [seconds, setSeconds] = useState(secs);
-  let [breakTime, setBreak] = useState(breakT);
-
+  //converting it to a 2 digit number
   let fSeconds = ("0" + seconds).slice(-2);
   let fminutes = ("0" + minutes).slice(-2);
 
   let time = "Focus Time";
 
+  //the update loop that runs every second
   useEffect(() => {
-    let interval = setInterval(() => {
+    if (start) {
+      let interval = setInterval(() => {
         clearInterval(interval);
         setSeconds(seconds - 1);
-        
-        if (seconds == 0)
-        {
-            setMinutes(minutes-1);
-            setSeconds(secs);
-        }
-        
-        if (!breakTime && seconds == 0 && minutes == 0)
-        {
-            setBreak(breakTime = true);
-            time = "break Time"
-            setMinutes(4);
-            setSeconds(60);
-        }
-        else if (breakTime && seconds == 0 && minutes == 0)
-        {
-            setBreak(breakTime = false);
-            time = "Focus Time"
-            setMinutes(24);
-            setSeconds(60);
+
+        if (seconds == 0) {
+          setMinutes(minutes - 1);
+          setSeconds(secs);
         }
 
-    }, 1000);
+        if (!breakTime && seconds == 0 && minutes == 0) {
+          setBreak(breakTime = true);
+          time = "break Time"
+          setMinutes(4);
+          setSeconds(60);
+        }
+        else if (breakTime && seconds == 0 && minutes == 0) {
+          setBreak(breakTime = false);
+          time = "Focus Time"
+          setMinutes(24);
+          setSeconds(60);
+        }
+
+      }, 1000);
+    }
   }, [minutes, seconds]);
-    
-  
+
+  //button functions has a bug h
+  const startfunc = () => {
+    setMinutes(24);
+    setSeconds(60);
+    setStart(true);
+  }
+
+  const stopfunc = () => {
+    setMinutes(0);
+    setSeconds(0);
+    setStart(false);
+  }
 
   return (
     <>
       <h1>Timer</h1>
       <p>{time}</p>
       <p>{fminutes}:{fSeconds}</p>
-      <button className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">start</button>
+      <button onClick={startfunc} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">start</button>
+
+      <button onClick={stopfunc} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-lg hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80">stop</button>
 
     </>
   )
